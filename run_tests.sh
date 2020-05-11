a4a6910087e3be81a831a89aba11e3112e15ffb6
Scott Henderson <scottyh@uw.edu>
2020-05-11 13:52:24 -0700

    install pytest with conda

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,8 @@
 # Usage: docker run -v $PWD:/home/jovyan pangeodev/base-notebook:master ./run_tests.sh base-notebook
 echo "Testing docker image {$1}..."
 
-pip install pytest
+#pip install pytest
+conda install pytest -y
 
 pytest -v tests/test_all.py tests/test_$1.py
 
