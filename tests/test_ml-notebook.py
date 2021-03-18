8ca258d429b6a0627ad339448f89eba7d5ff4878
Ryan Abernathey <ryan.abernathey@gmail.com>
2021-03-18 19:22:09 -0400

    add jax to ML notebook

--- tests/test_ml-notebook.py
+++ tests/test_ml-notebook.py
@@ -6,6 +6,7 @@ import os
 packages = [
     # machine learning stuff
     'tensorflow', 'skimage', 'sklearn',
+    'jax',
     # cupy import fails unless on GPU-enabled node:
     #'cupy', #libcuda.so.1: cannot open shared object file: No such file or directory
     # Error w/conda solve of pytorch+tensorflow, so not currently installed
