3de8187b3cb99f272586e4a86707064c928cc53f
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-04-26 12:39:33 -0400

    add torchgeo

--- tests/test_ml-notebook.py
+++ tests/test_ml-notebook.py
@@ -6,11 +6,9 @@ import os
 packages = [
     # machine learning stuff
     'tensorflow', 'skimage', 'sklearn',
-    'jax', 'torch'
+    'jax', 'torch', 'torgcheo'
     # cupy import fails unless on GPU-enabled node:
     #'cupy', #libcuda.so.1: cannot open shared object file: No such file or directory
-    # Error w/conda solve of pytorch+tensorflow, so not currently installed
-    # 'torch'
     ]
 
 @pytest.mark.parametrize('package_name', packages, ids=packages)
