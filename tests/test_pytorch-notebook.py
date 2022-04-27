3acef32a4f614726a502068446bd6c93e891dfb9
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-04-27 19:10:26 -0400

    Ensure torch uses mkl, and remove redundant lines

--- tests/test_pytorch-notebook.py
+++ tests/test_pytorch-notebook.py
@@ -10,8 +10,6 @@ packages = [
     "sklearn",
     "torch",
     "torchgeo",
-    # cupy import fails unless on GPU-enabled node:
-    #'cupy', #libcuda.so.1: cannot open shared object file: No such file or directory
 ]
 
 
@@ -24,3 +22,11 @@ def test_start():
     print(os.environ)
     if os.environ.get("PANGEO_ENV") is not None:
         assert os.environ["PANGEO_ENV"] == "pytorch-notebook"
+
+
+def test_torch_uses_mkl():
+
+    import torch
+
+    blas_info_index = torch.__config__.show().find("BLAS_INFO")
+    assert torch.__config__.show()[blas_info_index + 10 : blas_info_index + 13] == "mkl"
