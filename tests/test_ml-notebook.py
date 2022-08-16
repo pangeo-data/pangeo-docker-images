b95f38b93b4751569cc6f9708c4868036906874f
ngam <67342040+ngam@users.noreply.github.com>
2022-08-16 17:33:57 -0400

    add more tests to tf + jax

--- tests/test_ml-notebook.py
+++ tests/test_ml-notebook.py
@@ -6,7 +6,7 @@ import os
 packages = [
     # machine learning stuff
     'tensorflow', 'skimage', 'sklearn',
-    'jax',
+    'jax', 'tensorflow, jax', 
     # cupy import fails unless on GPU-enabled node:
     #'cupy', #libcuda.so.1: cannot open shared object file: No such file or directory
     # Error w/conda solve of pytorch+tensorflow, so not currently installed
