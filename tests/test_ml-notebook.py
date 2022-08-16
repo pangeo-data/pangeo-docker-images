82670a1acfe735c35c9bb3d803266f4dc861a052
ngam <67342040+ngam@users.noreply.github.com>
2022-08-16 18:13:39 -0400

    hopefully a better test

--- tests/test_ml-notebook.py
+++ tests/test_ml-notebook.py
@@ -6,7 +6,7 @@ import os
 packages = [
     # machine learning stuff
     'tensorflow', 'skimage', 'sklearn',
-    'jax', 'tensorflow, jax', 
+    'jax',
     # cupy import fails unless on GPU-enabled node:
     #'cupy', #libcuda.so.1: cannot open shared object file: No such file or directory
     # Error w/conda solve of pytorch+tensorflow, so not currently installed
@@ -21,3 +21,12 @@ def test_start():
     print(os.environ)
     if os.environ.get('PANGEO_ENV') is not None:
         assert os.environ['PANGEO_ENV'] == 'ml-notebook'
+
+def test_jax_tf_together():
+    """ sometimes this impport fails due to sharing private symbols
+        complicated longer story, but it is better to
+        ensure they can coexist
+    """
+    import tensorflow, jax
+    assert tensorflow.__version__[0] >= 2
+    assert jax.__version__[0] >= 0
