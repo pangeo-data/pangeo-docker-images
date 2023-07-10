31e6be07a195464dfc9bde0f064839a509f396cd
YuviPanda <yuvipanda@gmail.com>
2023-07-10 15:16:18 -0700

    Initial readthedocs setup

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+project = 'Pangeo Docker Stacks'
+copyright = '2023, Pangeo Contributors'
+author = 'Pangeo Contributors'
+
+
+extensions = [
+    "myst_parser",
+]
+
+myst_enable_extensions = [
+    "deflist",
+    "colon_fence",
+]
+
+source_suffix = [".rst", ".md"]
+
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_book_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# Welcome to Pangeo Docker Stacks documentation!
+
+![Build Status](https://github.com/pangeo-data/pangeo-docker-images/workflows/Build/badge.svg)
+![Publish Status](https://github.com/pangeo-data/pangeo-docker-images/workflows/Publish/badge.svg)
+![DockerHub Version](https://img.shields.io/docker/v/pangeo/base-image?sort=date)
+
+Curated sets of docker images for doing earth data analysis!
